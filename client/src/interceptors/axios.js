024990d112db6bff84347fea94ea491f41e0592d
solaialagu10 <hemapavi@gmail.com>
2023-07-25 23:03:10 +0530

    New web services url

--- client/src/interceptors/axios.js
+++ client/src/interceptors/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://billing-server-6s37.onrender.com';
+axios.defaults.baseURL = 'https://billing-server-x3d4.onrender.com';
 axios.interceptors.request.use(function (config) {
     let token = document.cookie.replace(/(?:(?:^|.*;\s*)_auth\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     config.headers.Authorization =  `bearer ${token}`;     
