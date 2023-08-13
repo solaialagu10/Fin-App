3a6f186d7bcb2c556b852fbd9a484804f5e9edfd
solaialagu10 <hemapavi@gmail.com>
2023-08-13 11:33:08 +0530

    Changed api url

--- client/src/interceptors/axios.js
+++ client/src/interceptors/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.baseURL = 'https://billing-server-x3d4.onrender.com';
 axios.interceptors.request.use(function (config) {
     let token = document.cookie.replace(/(?:(?:^|.*;\s*)_auth\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     config.headers.Authorization =  `bearer ${token}`;     
